98ce5e901b6d122dcb70e14f57d28fd5ec7ca6c6
mynul-islam-madhurjo <iammynul.bracu@gmail.com>
2024-12-05 15:19:00 +0600

    Data bindings

---
+++ src/app/rooms/rooms.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rooms',
+  templateUrl: './rooms.component.html',
+  styleUrls: ['./rooms.component.scss']
+})
+export class RoomsComponent implements OnInit {
+  hotelName  = 'Hilton Hotel';
+  numberOfRooms = 10;
+  hideRooms = false;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  toggle() {
+    this.hideRooms = !this.hideRooms;
+  }
+
+}
